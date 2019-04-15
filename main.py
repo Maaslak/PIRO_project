5f34f591d508911b44d426843a1a837ba253edb8
yaxlie <m.lesnierowski@gmail.com>
2019-04-15 22:06:48 +0200

    Add contour points detector

--- main.py
+++ main.py
@@ -5,7 +5,40 @@ import numpy as np
 
 from skimage.transform import probabilistic_hough_line
 from skimage import feature
+from skimage import transform
+from skimage import measure
 from skimage.morphology import disk, dilation
+from skimage.draw import polygon
+
+CONTOUR_PRECISION = 2
+IMAGE_SIZE = 100
+
+
+class Image:
+    def __init__(self, image):
+        self.image = transform.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+        self.contour_points = self.detect_contour()
+
+    def detect_contour(self):
+        edges = feature.canny(self.image, 1)
+        edges = dilation(edges, disk(2))
+
+        # Zamiast edges chyba można dać po prostu image
+        contours = measure.find_contours(edges, 0)
+        # Najbardziej odpowiedni kontur znajduje się na pozycji 0
+        contour = contours[0]
+
+        coords = measure.approximate_polygon(contour, CONTOUR_PRECISION)
+        contour_image = np.zeros([IMAGE_SIZE, IMAGE_SIZE, 3], dtype=np.uint8)
+        plt.plot(coords[:, 1], coords[:, 0], '-w', linewidth=2)
+        # contour_image[coords[:, 1]][coords[:, 0]] = 1
+        # print(coords[:, 1], coords[:, 0])
+        # rr, cc = polygon(coords[:, 1], coords[:, 0])
+        #
+        # contour_image[rr, cc] = 1
+        plt.imshow(contour_image)
+        plt.show()
+        return coords
 
 
 def lines_to_vec(lines):
@@ -45,7 +78,7 @@ def find_base(image):
          plt.plot((p0[0], p1[0]), (p0[1], p1[1]), linewidth=3)
 
     plt.imshow(edges, cmap=plt.cm.gray)
-    plt.show()
+    # plt.show()
 
 
 def vectorize_image(image):
@@ -55,7 +88,9 @@ def vectorize_image(image):
 def run_alg(data_dir, set_no):
     for img_no in range(set_no):
         image = imread("{}/{}.png".format(data_dir, img_no))
-        vectorize_image(image)
+        # vectorize_image(image)
+
+        _image = Image(image)
 
 
 if __name__ == "__main__":
